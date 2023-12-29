82cd270c1d9387f1b84b928666e47e9d5e4dfb0d
freckled <jabbarovkamoladdin@gmail.com>
2023-12-29 23:28:37 +0500

    added consent bar

---
+++ dist/config.js
@@ -0,0 +1,90 @@
+var klaroConfig = {
+    version: 1,
+
+    elementID: 'klaro',
+    styling: {
+        theme: ['light', 'top', 'wide'],
+    },
+    noAutoLoad: false,
+    htmlTexts: true,
+    embedded: false,
+    groupByPurpose: true,
+    storageMethod: 'cookie',
+    cookieName: 'klaro',
+    cookieExpiresAfterDays: 365,
+    default: false,
+    mustConsent: false,
+    acceptAll: true,
+    hideDeclineAll: false,
+    hideLearnMore: false,
+    noticeAsModal: false,
+
+    translations: {
+        en: {
+            consentModal: {
+                title: '<u>test</u>',
+                description:
+                    'Here you can see and customize the information that we collect about you. Entries marked as "Example" are just for demonstration purposes and are not really used on this website.',
+            },
+            inlineTracker: {
+                description: 'Example of an inline tracking script',
+            },
+            externalTracker: {
+                description: 'Example of an external tracking script',
+            },
+            adsense: {
+                description: 'Displaying of advertisements (just an example)',
+                title: 'Google Adsense Advertisement',
+            },
+            matomo: {
+                description: 'Collecting of visitor statistics',
+            },
+            camera: {
+                description:
+                    'A surveillance camera (just an example for an IMG tag)',
+            },
+            cloudflare: {
+                description: 'Protection against DDoS attacks',
+            },
+            intercom: {
+                description:
+                    'Chat widget & collecting of visitor statistics (just an example)',
+            },
+            mouseflow: {
+                description: 'Real-Time user analytics (just an example)',
+            },
+            googleFonts: {
+                description: 'Web fonts hosted by Google',
+            },
+            purposes: {
+                analytics: 'Analytics',
+                security: 'Security',
+                livechat: 'Livechat',
+                advertising: 'Advertising',
+                styling: 'Styling',
+            },
+        },
+    },
+    services: [
+        {
+            name: 'Google Ads',
+            default: false,
+            purposes: ['marketing'],
+        },
+        {
+            name: 'Facebook Ads',
+            default: false,
+            purposes: ['marketing'],
+        },
+        {
+            name: 'Google Analytics',
+            default: false,
+            title: 'Google Analytics',
+            purposes: ['marketing'],
+            cookies: ['_ga']
+        },
+
+    ],
+
+};
+
